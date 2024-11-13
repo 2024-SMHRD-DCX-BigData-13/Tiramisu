0c2181cd8577b9c89021fb7b4836a6169af1becb
smhrd <smhrd@a>
2024-11-13 17:43:34 +0900

    1113 이혜지	드래그수정

--- bangkok/src/main/webapp/assets/js/ie/i.js
+++ bangkok/src/main/webapp/assets/js/ie/i.js
@@ -8,10 +8,8 @@ $(document).ready(function() {
         $('.slide-overlay').stop(true, true).fadeOut(100);
       }
     );
-});
 
 //
-$(document).ready(function() {
     let imgWidth = $(".slide-item").outerWidth();
     let slideCount = $(".slide-img").length;
     let currentIndex = 0;
@@ -47,15 +45,16 @@ $(document).ready(function() {
         imgWidth = $(".slide-item").outerWidth();
         slideToIndex(currentIndex);
     });
-});
+
 
 
 //
-$(document).ready(function() {
     var isDragging = false;
     var startPosition = 0;
     var currentTranslate = 0;
     var previousTranslate = 0;
+    
+    console.log(totalItems);
     var totalItems = $(".theme-item li").length;
     const slider = $(".theme-item");
     const progressBar = $(".progress-b");
@@ -82,25 +81,7 @@ $(document).ready(function() {
         slider.css("transform", "translateX(" + currentTranslate + "px)");
         updateProgressBar();
     });
-
-    slider.on("mouseup", function(e) {
-        if (!isDragging) return;
-        isDragging = false;
-        slider.css("cursor", "grab");
-
-        var maxTranslate = -(totalItems - 1) * slider[0].getBoundingClientRect().width;
-        if (currentTranslate > 0) {
-            currentTranslate = 0;
-        } else if (currentTranslate < maxTranslate) {
-            currentTranslate = maxTranslate;
-        }
-
-        previousTranslate = currentTranslate;
-        slider.css("transform", "translateX(" + currentTranslate + "px)");
-        updateProgressBar();
-    });
-
-    slider.on("mouseleave", function() {
+    slider.on("mouseleave mouseup", function() {
         if (isDragging) {
             isDragging = false;
             slider.css("cursor", "grab");
